501434fe556f2bbfcf6d576769358ce54238a30f
tongyuan <3105609687@qq.com>
2018-12-03 06:31:14 +0000

    add from validate class

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\DemoRequest;
 
 class UsersController extends Controller
 {
-    public function create()
+    public function create(DemoRequest $request)
     {
 	return view('users.create');
     }	    
