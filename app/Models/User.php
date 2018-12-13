1de14a223d09e30687215238c65082dbe241543c
tongyuan <tongyuanzhao@aijiatui.com>
2018-12-13 10:36:54 +0800

    修复程序

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Notifications\ResetPassword;
+use Auth;
 
 
 class User extends Authenticatable
@@ -58,7 +59,7 @@ class User extends Authenticatable
     {
         $user_ids = Auth::user()->followings->pluck('id')->toArray();
         array_push($user_ids,Auth::user()->id);
-        return Status::whereIn('user)id',$user_ids)->with('user')->orderBy('created_at','desc');
+        return Status::whereIn('user_id',$user_ids)->with('user')->orderBy('created_at','desc');
     }
 
     public function followers()
